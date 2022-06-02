725bf4ff1af7a56cfa01dd78cd6d3967dfa837b6
Kavish Rizvi <rizvikavish205.com>
2022-06-02 12:52:39 +0530

    First Commit

---
+++ src/test/java/com/proofofconcept/messagingqueue/MessagingQueueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proofofconcept.messagingqueue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagingQueueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
